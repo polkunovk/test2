c8d452d025a3fc65cde789787d426613011073b8
Konstantin <lenaaks@mail.ru>
2024-09-28 01:42:11 +0300

    11 спринт

--- src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
@@ -6,10 +6,15 @@ import java.util.List;
 import java.util.Set;
 
 public interface UserStorage {
+
     User addUser(User user);
+
     User updateUser(User user);
+
     void deleteUser(int id);
+
     List<User> getAllUsers();
+
     User getUserById(int id);
 
     List<User> getUsersByIds(Set<Long> ids);
