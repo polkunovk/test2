39b853f1ba0badabcd08556dda53020794a56478
Konstantin <lenaaks@mail.ru>
2024-09-28 01:40:48 +0300

    11 спринт

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -4,10 +4,15 @@ import ru.yandex.practicum.filmorate.model.Film;
 import java.util.List;
 
 public interface FilmStorage {
+
     Film addFilm(Film film);
+
     Film updateFilm(Film film);
+
     void deleteFilm(int id);
+
     List<Film> getAllFilms();
+
     Film getFilmById(int id);
 
     List<Film> getPopularFilms(int count);
